fb04769d23673d3e22e6dfc0e055b590b0d1ce02
MiewONE <dlsrk489@gmail.com>
2020-07-06 19:39:56 +0900

    상태페이지 UI구현, 기능구현 필요/ 통계 그래프 수정 필요

--- src/components/State/DataState.js
+++ src/components/State/DataState.js
@@ -11,6 +11,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
 const useStyles = makeStyles((theme) => ({
     root: {
         backgroundColor: '#f3f3f3',
@@ -38,19 +39,19 @@ const DataState = () => {
     const graphwidth = '20%';
     return (
         <div style={{ backgroundColor: '#f3f3f3' }}>
-            <div style={{ margin: '0% 2% 2% 2%' }}>
-                <span > 
+            <div style={{ margin: '0% 2% 2% 2%',marginLeft:'40%' }}>
+                <span> 
                     <br />
                     현재 시스템의 상태를 보여줍니다.
                 </span>
             </div>
-            <div style={{ display: 'inline-block', backgroundColor: 'white', margin: '0% 0% 5% 5%', width: '30%' }}>
+            <div style={{ display: 'inline-block', backgroundColor: 'white', margin: '0% 0% 5% 5%', width: '50%' }}>
                 <Paper
                     elevation={3}
                     variant="outlined"
                 >
                     <span class="data_span">
-                        온도와 습도
+                     온습도
                         </span>
                     <div>
                         <div class="div_chart">
@@ -96,43 +97,66 @@ const DataState = () => {
 
                 </Paper>
             </div>
-            <div style={{ display: 'inline-block', backgroundColor: 'white', margin: '0% 5% 5% 2%', width: '50%' }}>
+            <div style={{ display: 'inline-block', backgroundColor: 'white', margin: '0% 5% 5% 5%', width: '30%' }}>
+            <Paper
+                    elevation={3}
+                    variant="outlined"
+                >
+                <img src="https://tekladesign.net/wp-content/uploads/2017/03/server.png" alt="server" class="server_icon"/><br/>
+                <span style={{fontSize:'2em',margin:'0% 0% 10% 10%'}}>
+                서버 정상 작동중
+                </span>
+                
+                </Paper>
+            </div>
+            <div style={{ display: 'inline-block', backgroundColor: 'white', margin: '0% 5% 5% 5%', width: '90%' }}>
+            <Paper
+                    elevation={3}
+                    variant="outlined"
+                >
                 <Table>
                     <TableHead>
                         <TableRow>
-                                <TableCell>
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLv6QwhaahoPicmw_nNNwNUq467NaSwFA6YA&usqp=CAU" alt="pump" /><br/>
+                                <TableCell></TableCell>
+                                <TableCell align="center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLv6QwhaahoPicmw_nNNwNUq467NaSwFA6YA&usqp=CAU" alt="pump" class="img_head"/><br/>
                                     펌프
                                 </TableCell>
-                                <TableCell>
-                                    <img src="https://i.ibb.co/p1w6hYw/led.jpg" alt="LED"/>
+                                <TableCell align="center">
+                                    <img src="https://i.ibb.co/p1w6hYw/led.jpg" alt="LED" class="img_head"/><br/>
                                     LED
                                 </TableCell>
-                                <TableCell>
+                                <TableCell align="center">
+                                    <img src="https://www.sifmdc.com/uploads/preheater_sm.png" alt="preheater" class="img_head"/><br/>
                                     난방기
                                 </TableCell>
-                                <TableCell>
+                                <TableCell align="center">
+                                    <img src="https://us.123rf.com/450wm/mr_vector/mr_vector1603/mr_vector160303563/52956169-%EC%9D%B8%EA%B3%B5-%ED%98%B8%ED%9D%A1%EA%B8%B0-%EC%97%90%EC%96%B4%EC%BB%A8-%EB%B2%A1%ED%84%B0-%EC%95%84%EC%9D%B4%EC%BD%98.jpg?ver=" alt="fan" class="img_head"/> <br/>
                                     팬
                                 </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         <TableRow>
-                                <TableCell>
-                                    펌프
+                                <TableCell align="center">
+                                    상태
                                 </TableCell>
-                                <TableCell>
-                                    LED
+                                <TableCell align="center">
+                                    <img src="https://i.ibb.co/hfZMHtJ/on.png" alt="off" />
                                 </TableCell>
-                                <TableCell>
-                                    난방기
+                                <TableCell align="center">
+                                    <img src="https://i.ibb.co/NZPGRk7/off.png" alt="on"/>
                                 </TableCell>
-                                <TableCell>
-                                    팬
+                                <TableCell align="center">
+                                    <img src="https://i.ibb.co/hfZMHtJ/on.png" alt="off" />
+                                </TableCell>
+                                <TableCell align="center">
+                                    <img src="https://i.ibb.co/hfZMHtJ/on.png" alt="off" />
                                 </TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
+                </Paper>
             </div>
         </div>
     );
--- src/components/State/Graph.js
+++ src/components/State/Graph.js
@@ -6,21 +6,27 @@ import {
     LineSeries,
     SplineSeries,
   } from '@devexpress/dx-react-chart-material-ui';
-const generateData = (start, end, step) => {
-const data = [];
-for (let i = start; i < end; i += step) {
-    data.push({ splineValue: Math.sin(i) / i, lineValue: ((i / 15) ** 2.718) - 0.2, argument: i });
-}
-
-return data;
+const generateData = () => {
+    const data= [];
+// for (let i = start; i < end; i += step) {
+//     data.push({ splineValue: i, lineValue: i*2, argument: i+2 });
+// }
+    const temp=[35,35,35,35,35,35,35,35,35,35,35,36,36,36,36,36,36,37,37,37,37,37,37,37,37,37];    
+    const humi=[20,20,20,20,20,20,20,20,20,20,25,25,25,25,25,25,25,23,23,23,23,23,23,23,32,32];
+    // const dats=['2020-07-02 06:37:21','2020-07-02 06:45:04','2020-07-02 오후 3:49:59','2020-07-02 오후 3:55:00','2020-07-02 오후 3:55:01','2020-07-02 오후 3:55:03','2020-07-02 오후 3:55:04','2020-07-02 오후 3:55:05','2020-07-02 오후 3:55:07','2020-07-02 오후 3:55:08','2020-07-02 오후 3:55:10','2020-07-02 오후 3:55:11','2020-07-02 오후 3:55:12','2020-07-02 오후 3:55:14','2020-07-02 오후 3:55:15','2020-07-02 오후 3:55:17','2020-07-02 오후 3:55:18','2020-07-02 오후 3:55:19','2020-07-02 오후 3:55:21','2020-07-02 오후 3:55:22','2020-07-02 오후 3:55:24','2020-07-02 오후 3:55:25','2020-07-02 오후 3:55:26','2020-07-02 오후 3:55:28','2020-07-02 오후 3:55:29','2020-07-02 오후 3:55:31'];
+    for(let i=0;i<26;i=i+1){
+        data.push({splineValue:temp[i],lineValue:humi[i],argument:i})
+    }
+    return data;
 };
 const state = {
-    data: generateData(2.5, 12, 0.1),
+    data: generateData()
   };
 const Graph = () => {
     const { data: chartData } = state;
     return (
         <div>
+            <div style={{width:"80%",margin:'10% 10% 0% 10%'}}>
             <Chart
                 data={chartData}
             >
@@ -30,6 +36,7 @@ const Graph = () => {
                 name="line"
                 valueField="lineValue"
                 argumentField="argument"
+                
             />
             <SplineSeries
                 name="spline"
@@ -37,6 +44,10 @@ const Graph = () => {
                 argumentField="argument"
             />
             </Chart>
+            </div>
+            <div>
+                빨간 : 온도 / 파랑 : 습도
+            </div>
         </div>
     );
 };
